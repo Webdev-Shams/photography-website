7505957365e0fe8436a329ef44836ad0ec05d053
Webdev-Shams <webdev.shams@gmail.com>
2022-10-20 02:16:04 +0600

    not found page text centered vertically

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,9 +4,11 @@ import { FaRegSadTear } from "react-icons/fa";
 
 const NotFound = () => {
     return (
-        <div className='pt-32 font-light text-center text-5xl md:text-6xl'>
-            <h1 className='mb-9'>NOT FOUND <span className='font-bold text-red-500'>404</span>!</h1>
-            <p className='inline-block text-slate-200 text-9xl'><FaRegSadTear></FaRegSadTear></p>
+        <div className='grid grid-cols-1 items-center pt-32 font-light text-center text-5xl md:text-6xl h-screen'>
+            <div>
+                <h1 className='mb-9'>NOT FOUND <span className='font-bold text-red-500'>404</span>!</h1>
+                <p className='inline-block text-slate-200 text-9xl'><FaRegSadTear></FaRegSadTear></p>
+            </div>
         </div>
     );
 };
